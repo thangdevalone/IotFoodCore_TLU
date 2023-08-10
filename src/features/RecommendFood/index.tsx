01f4085dd2ebc9fd0db7315bd6bfbe3f6c5a511b
haidang <haidang02032003@gmail.com>
2023-08-10 22:00:14 +0700

    rec && type Food

--- src/features/RecommendFood/index.tsx
+++ src/features/RecommendFood/index.tsx
@@ -1,7 +1,7 @@
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography, Grid } from "@mui/material";
 import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import * as React from "react";
 import foodsApis from "@/api/foodsApi";
@@ -16,16 +16,7 @@ export interface RecommendFoodProps {}
 
 export function RecommendFood(props: RecommendFoodProps) {
   const [data, setData] = React.useState<RecommendFoodData[]>([])
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const response = await foodsApis.getRecommendFood()
-      if (response?.status) {
-        setData(response?.data)
-      }
-    }
-    fetchData()
-  }, [])
+  const [seeAll, setSeeAll] = React.useState<boolean>(true);
 
   const swiperRef = React.useRef<any>(null)
 
@@ -56,80 +47,139 @@ export function RecommendFood(props: RecommendFoodProps) {
   //
 
   return (
-    <div className="flex gap-3 mt-8">
-      {width > 600 && 
-        <Box
-        className="flex items-center justify-center cursor-pointer">
-          <IconButton onClick={slidePrev}>
-          <ChevronLeft fontSize="large" />
-        </IconButton>
-      </Box>
-      }
-      <Swiper
-        modules={[]}
-        loop={true}
-        style={{ width: "100%" }}
-        slidesPerView={width <= 600 ? 1.2 : 4}
-        spaceBetween={25}
-        allowTouchMove={true}
-        ref={swiperRef}
-      >
-        {data?.map((item, index) => (
-          <SwiperSlide key={index + item?.id}>
+    <>
+      {seeAll ? 
+        <Box className="flex gap-3 mt-8">
+          {width > 600 && 
             <Box
-              className="w-screen rounded-md cursor-pointer object-cover"
-              sx={{
-                width: `${width < 601 ? '38vh' : '45vh'}`,
-                maxHeight: `${width < 601 ? '150px' : '200px'}`,
-                minHeight: `${width < 601 ? '150px' : '200px'}`,
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundImage: `url(${item.imgFood})`,
-              }}
-            ></Box>
-            <Stack className="">
-              <span className="text-lg font-semibold capitalize">
-                {item.nameRestaurantFood}
-              </span>
-              <Box className="flex gap-5 capitalize">
-                <Typography>{item.nameFood}</Typography>
-                <Typography className="text-gray-400">
-                  {handlePrice(item.price)} VND
-                </Typography>
-              </Box>
-              <Box className="flex gap-10 items-center mt-2">
-                <Typography className="text-[20px] flex  justify-center items-center">
-                  <StarRateRoundedIcon
-                    style={{ color: "orange", width: "30px", height: "30px" }}
-                  />
-                  <Typography> {item.star} </Typography>
-                </Typography>
-                <Box className="flex gap-2">
-                  <Typography className="flex items-center justify-center gap-2">
-                    <AccessTimeRoundedIcon
-                      style={{ width: "30px", height: "30px" }}
-                    />
-                    {item.time} phút
-                  </Typography>
-                  <FiberManualRecordIcon
-                    style={{ width: "9px", height: "9px", marginTop: "10px" }}
-                  />
-                  <Typography className="flex items-center justify-center">
-                    {item.distance} km
-                  </Typography>
-                </Box>
-              </Box>
-            </Stack>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {width > 600 && 
-        <Box className="flex items-center justify-center">
-          <IconButton onClick={slideNext} >
-            <ChevronRight fontSize="large" />
-          </IconButton>
-      </Box>
-      }
-    </div>
+              className="flex items-center justify-center cursor-pointer">
+                <IconButton onClick={slidePrev}>
+                <ChevronLeft fontSize="large" />
+              </IconButton>
+            </Box>
+          }
+          <Swiper
+            modules={[]}
+            loop={true}
+            style={{ width: "100%" }}
+            slidesPerView={width <= 600 ? 1.2 : 4}
+            spaceBetween={25}
+            allowTouchMove={true}
+            ref={swiperRef}
+          >
+            {data?.map((item, index) => (
+              <SwiperSlide key={index + item?.id}>
+                <Box
+                  className="w-screen rounded-md cursor-pointer object-cover"
+                  sx={{
+                    width: `${width < 601 ? '38vh' : '45vh'}`,
+                    maxHeight: `${width < 601 ? '150px' : '200px'}`,
+                    minHeight: `${width < 601 ? '150px' : '200px'}`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${item.imgFood})`,
+                  }}
+                ></Box>
+                <Stack className="">
+                  <span className="text-lg font-semibold capitalize">
+                    {item.nameRestaurantFood}
+                  </span>
+                  <Box className="flex gap-5 capitalize">
+                    <Typography>{item.nameFood}</Typography>
+                    <Typography className="text-gray-400">
+                      {handlePrice(item.price)} VND
+                    </Typography>
+                  </Box>
+                  <Box className="flex gap-10 items-center mt-2">
+                    <Typography className="text-[20px] flex  justify-center items-center">
+                      <StarRateRoundedIcon
+                        style={{ color: "orange", width: "30px", height: "30px" }}
+                      />
+                      <Typography> {item.star} </Typography>
+                    </Typography>
+                    <Box className="flex gap-2">
+                      <Typography className="flex items-center justify-center gap-2">
+                        <AccessTimeRoundedIcon
+                          style={{ width: "30px", height: "30px" }}
+                        />
+                        {item.time} phút
+                      </Typography>
+                      <FiberManualRecordIcon
+                        style={{ width: "9px", height: "9px", marginTop: "10px" }}
+                      />
+                      <Typography className="flex items-center justify-center">
+                        {item.distance} km
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Stack>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {width > 600 && 
+            <Box className="flex items-center justify-center">
+              <IconButton onClick={slideNext} >
+                <ChevronRight fontSize="large" />
+              </IconButton>
+            </Box>
+          }
+        </Box>
+        :
+        <Stack className="mx-16 mt-8 items-center justify-center">
+          <Grid className="" container spacing={4} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
+            {data?.map(item =>
+              <Grid item xs={12} sm={6} md={3} key={item.id}>
+                <Box
+                  className="w-screen rounded-md cursor-pointer object-cover"
+                  sx={{
+                    width: `${width < 601 ? '38vh' : '45vh'}`,
+                    maxHeight: `${width < 601 ? '150px' : '200px'}`,
+                    minHeight: `${width < 601 ? '150px' : '200px'}`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${item.imgFood})`,
+                  }}
+                ></Box>
+                <Stack className="">
+                  <span className="text-lg font-semibold capitalize">
+                    {item.nameRestaurantFood}
+                  </span>
+                  <Box className="flex gap-5 capitalize">
+                    <Typography>{item.nameFood}</Typography>
+                    <Typography className="text-gray-400">
+                      {handlePrice(item.price)} VND
+                    </Typography>
+                  </Box>
+                  <Box className="flex gap-10 items-center mt-2">
+                    <Typography className="text-[20px] flex  justify-center items-center">
+                      <StarRateRoundedIcon
+                        style={{ color: "orange", width: "30px", height: "30px" }}
+                      />
+                      <Typography> {item.star} </Typography>
+                    </Typography>
+                    <Box className="flex gap-2">
+                      <Typography className="flex items-center justify-center gap-2">
+                        <AccessTimeRoundedIcon
+                          style={{ width: "30px", height: "30px" }}
+                        />
+                        {item.time} phút
+                      </Typography>
+                      <FiberManualRecordIcon
+                        style={{ width: "9px", height: "9px", marginTop: "10px" }}
+                      />
+                      <Typography className="flex items-center justify-center">
+                        {item.distance} km
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Stack>
+              </Grid>
+            )}
+          </Grid>
+        </Stack>}
+      <Stack onClick={()=> setSeeAll(!seeAll)} className="my-5 items-center justify-center text-2xl font-medium cursor-pointer py-2 border-[2px] mx-16 rounded-lg hover:bg-gray-100">
+        See all promotions
+      </Stack>
+    </>
   )
 }
--- src/features/TypeFood/index.tsx
+++ src/features/TypeFood/index.tsx
@@ -16,18 +16,21 @@ const TypeFood = () => {
   }, []);
   
   return (
-    <Box className="flex items-center justify-center my-5 lg:mx-16">
-      <Grid className="" container spacing={4} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
-        {data?.map(item =>
-          <Grid item xs={12} sm={6} md={3} key={item.id}>
-            <Box className="flex flex-col items-center justify-center">
-              <div className="h-[150px] w-[300px] lg:w-[400px] bg-gray-300"></div>
-            </Box>
-            <span className='text-xl font-semibold flex justify-start lg:ml-3 ml-[44px]'>{item.nameType}</span>
-          </Grid>
-        )}
-      </Grid>
-    </Box>
+    <>
+      <h1 className='lg:mx-16 text-2xl font-bold'>There's something for everyone!</h1>
+      <Box className="flex items-center justify-center my-5 lg:mx-16">
+        <Grid className="" container spacing={4} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
+          {data?.map(item =>
+            <Grid item xs={12} sm={6} md={3} key={item.id}>
+              <Box className="flex flex-col items-center justify-center">
+                <div className="h-[150px] w-[300px] lg:w-[380px] bg-gray-300"></div>
+              </Box>
+              <span className='text-xl font-semibold flex justify-start lg:ml-0 ml-[44px]'>{item.nameType}</span>
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </>
   )
 }
 
