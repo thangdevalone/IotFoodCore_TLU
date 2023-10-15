e38c7a63f9f1925af288a9d0bb3a37b959bfec65
ThangDevAlone <quangthangvtlg@gmail.com>
2023-10-15 21:31:16 +0700

    hehe

--- src/components/ProtectRouter/ProtectCheckout.tsx
+++ src/components/ProtectRouter/ProtectCheckout.tsx
@@ -9,26 +9,38 @@ export function ProtectCheckout() {
   // time là hiện tại 9:00 PM
   const time = dayjs(new Date()).format("hh:mm A")
   const timeDeliver = useAppSelector((state) => state.cart.timeDeliver) // 11:00 AM
-  return !!user ? (
-    time.search("PM") !== -1 ? (
-      <>
-        Giờ này mình không ship nữa hic😞. Hãy quay lại vào ngày mai.{" "}
-        <a className="text-blue-600 hover:underline" href="/">
-          Quay lại trang chủ
-        </a>
-      </>
-    ) : isTimeDGTOneHour(timeDeliver, time) ? (
+  // return !!user ? (
+  //   time.search("PM") !== -1 ? (
+  //     <>
+  //       Giờ này mình không ship nữa hic😞. Hãy quay lại vào ngày mai.{" "}
+  //       <a className="text-blue-600 hover:underline" href="/">
+  //         Quay lại trang chủ
+  //       </a>
+  //     </>
+  //   ) : isTimeDGTOneHour(timeDeliver, time) ? (
+  //     <Checkout />
+  //   ) : (
+  //     <>
+  //       <b>Error</b>:Bạn chọn giờ giao là {timeDeliver}. Bạn phải đặt đơn trước
+  //       1 tiếng.{" "}
+  //       <a className="text-blue-600 hover:underline" href="/">
+  //         Quay lại trang chủ
+  //       </a>
+  //     </>
+  //   )
+  // ) : (
+  //   <>
+  //     Bạn cần{" "}
+  //     <a className="text-blue-600 hover:underline" href="/login">
+  //       đăng nhập
+  //     </a>{" "}
+  //     để tiếp tục với trang này
+  //   </>
+  // )
+  //-----------
+  return !!user ?
       <Checkout />
-    ) : (
-      <>
-        <b>Error</b>:Bạn chọn giờ giao là {timeDeliver}. Bạn phải đặt đơn trước
-        1 tiếng.{" "}
-        <a className="text-blue-600 hover:underline" href="/">
-          Quay lại trang chủ
-        </a>
-      </>
-    )
-  ) : (
+    : (
     <>
       Bạn cần{" "}
       <a className="text-blue-600 hover:underline" href="/login">
